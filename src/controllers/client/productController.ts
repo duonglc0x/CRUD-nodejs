cb12d5ce0c3719a2a15d02d30487f09bda011d37
Duong <lechauduong4@gmail.com>
2026-08-20 11:52:06 +0700

    Design the Add New Product Interface

--- src/controllers/client/productController.ts
+++ src/controllers/client/productController.ts
@@ -4,4 +4,8 @@ const getProductPage = async (req: Request, res: Response) => {
   return res.render("client/product/detail");
 };
 
+const getAdminCreateProductPage =async (req: Request, res: Response) => {
+  return res.render("client/product/create");
+};
+
 export {getProductPage}
--- src/routes/wed.ts
+++ src/routes/wed.ts
@@ -14,18 +14,23 @@ import {
   getAdminProductPage,
 } from "controllers/admin/dashBoardController";
 import { getProductPage } from "controllers/client/productController";
+import {
+  getAdminCreateProductPage,
+  postAdminCreateProduct,
+} from "controllers/admin/productController";
 import fileUploadMiddleware from "src/middleeware/multer";
 const router = express.Router();
 
 const wedRouter = (app: Express) => {
+  //client
   router.get("/", getHomePage);
-  router.get("/Product/:id",getProductPage);
+  router.get("/Product/:id", getProductPage);
 
   //admin
   router.get("/admin", getDashBoardPage);
   router.get("/admin/user", getAdminUserPage);
   router.get("/admin/order", getAdminOderPage);
-  router.get("/admin/product", getAdminProductPage);
+
   router.get("/admin/create-user", getCreateUserPage);
   router.post(
     "/admin/handle-create-user",
@@ -38,6 +43,14 @@ const wedRouter = (app: Express) => {
     "/admin/update-user/:id",
     fileUploadMiddleware("avatar"),
     postUpdateUser,
+  );
+
+  router.get("/admin/product", getAdminProductPage);
+  router.get("/admin/create-product", getAdminCreateProductPage);
+  router.post(
+    "/admin/handle-create-product",
+    fileUploadMiddleware("image", "images/product"),
+    postAdminCreateProduct,
   );
   app.use("/", router);
 };
